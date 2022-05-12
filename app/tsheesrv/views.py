604e5c286f287a6a73e4f0ff163b47ed9098eb58
Pavel Nikitin <p.doomkin@ya.ru>
2022-05-12 17:39:45 +0300

    Улучшен код

--- app/tsheesrv/views.py
+++ app/tsheesrv/views.py
@@ -6,9 +6,7 @@ from tools.cp1251 import decode_cp1251
 
 class SendView(web.View):
     async def post(self):
-        encoded = await self.request.content.read()
-        content = decode_cp1251(encoded)
-        org_code, org_inn = extract_org_code_inn(content)
+        content, org_code, org_inn = await self._extract_content()
         org = await find_org(org_code, org_inn)
         if org:
             result = await send_timesheet(org['db_key'], org['company_rn'], content)
@@ -17,3 +15,11 @@ class SendView(web.View):
             result = f'Учреждение "{org_code}" с ИНН {org_inn} не найдено'
             status = 422  # Unprocessable Entity
         return web.Response(text=f'{result}\n', status=status)
+
+    async def _extract_content(self):
+        encoded = await self.request.content.read()
+        decoded = decode_cp1251(encoded)
+        lines = decoded.splitlines()[4:-3]
+        content = '\n'.join(lines)
+        org_param = lines[1].split(';')
+        return content, org_param[0], org_param[1]
